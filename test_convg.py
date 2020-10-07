a4f22512377e7617290ae863d3424e1efb457e87
Keith Roberts <krober10@nd.edu>
2020-10-07 15:20:01 -0300

    cleaning up convg

--- test_convg.py
+++ test_convg.py
@@ -5,18 +5,18 @@ import numpy as np
 from benchmarker import solver_CG
 
 
-p = 1
+p = 2
 timestep = 0.0005
+space = "KMV"
 
 
 mesh = UnitSquareMesh(50, 50)
 mh = MeshHierarchy(mesh, 2)
-print(len(mh))
 sols = [
-    solver_CG(mesh, el="tria", space="CG", deg=p, T=0.50, dt=timestep) for mesh in mh
+    solver_CG(mesh, el="tria", space=space, deg=p, T=0.50, dt=timestep) for mesh in mh
 ]
 coarse_on_fine = []
-V = FunctionSpace(mh[-1], "CG", p)
+V = FunctionSpace(mh[-1], space, p)
 for sol in sols[:-1]:
     fine = Function(V).assign(sols[-1])
     prolong(sol, fine)
